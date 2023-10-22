fff729f0939bb9db7176a408c447e863f7ac97a4
andrewstellman <astellman@stellman-greene.com>
2023-10-22 17:18:40 -0400

    Added code to reproduce the problem

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		PrintButtonTexts();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+	
+        if (sender is Button buttonClicked)
+			buttonClicked.Text = String.Empty; // Changing this to any other string doesn't reproduce the problem
+
+		PrintButtonTexts();
+    }
+
+	private void PrintButtonTexts() {
+		ButtonText.Text = $"Button1.Text = '{Button1.Text}'{Environment.NewLine}"
+			+ $"Button2.Text = '{Button2.Text}'{Environment.NewLine}"
+			+ $"Button3.Text = '{Button3.Text}'{Environment.NewLine}"
+			+ $"Button4.Text = '{Button4.Text}'{Environment.NewLine}";
+	}
+}
+
